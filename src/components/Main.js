270e23bf17aac94fa01c790c486fa787974fffbe
chrisfyu <chrisfyu@hotmail.com>
2018-12-16 16:08:11 -0500

    add files, modify folder architecture

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yufei on 2018/12/12.
+ */
+import React, { Component } from 'react';
+import { Register } from './Register';
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div className="main">
+                <Register />
+            </div>
+        );
+    }
+}
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+/**
+ * Created by yufei on 2018/12/13.
+ */
+export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
